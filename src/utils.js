00ad183eff4915fe1b83b94c7686ff570003df4e
Nina <ninajane.boholst@gmail.com>
2025-03-31 21:34:17 +0800

    feature: integrate library.js submodule with utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,14 @@
 /******************************************************************************************************/
 
 import { getSecureRandomNumber } from '../hotfix/security-fix.js';
+import { getSecureRandomFloat, generateSecureUUID } from '../external-library/library.js';
 
 /******************************************************************************************************/
+
+/**
+ * Returns a secure random integer between min and max.
+ * Uses the main secure function from security-fix.js.
+ */
 
 function getRandomNumber(min, max) {
     return getSecureRandomNumber(min, max); // Use the secure generator
@@ -10,6 +16,28 @@ function getRandomNumber(min, max) {
 
 /******************************************************************************************************/
 
-export { getRandomNumber };
+/**
+ * Returns a secure random float between 0 and 1.
+ * Uses the external library.
+ */
+
+function getRandomFloat() {
+    return getSecureRandomFloat(); // Use library.js function
+}
+
+/******************************************************************************************************/
+
+/**
+ * Generates a secure UUID (v4).
+ * Uses the external library.
+ */
+
+function getUUID() {
+    return generateSecureUUID(); // Use library.js function
+}
+
+/******************************************************************************************************/
+
+export { getRandomNumber, getRandomFloat, getUUID };
 
 /******************************************************************************************************/
